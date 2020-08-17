537fadc97504ead500f27eb6a1bd419661e40081
Shuji Honda <22165429+hondash@users.noreply.github.com>
2020-08-17 10:35:42 +0900

    [ios-safari-100vh-issue] styling top page

--- ios-safari-100vh-issue/src/pages/top/TopPage.tsx
+++ ios-safari-100vh-issue/src/pages/top/TopPage.tsx
@@ -1,11 +1,34 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
 export const TopPage = () => {
   return (
-    <div>
-      <div><Link to="/flex">flex</Link></div>
-      <div><Link to="/grid">grid</Link></div>
-    </div>
+    <StyledTopPage>
+      <StyledLinkWrapper>
+        <Link to="/flex">flex</Link>
+      </StyledLinkWrapper>
+      <StyledLinkWrapper>
+        <Link to="/grid">grid</Link>
+      </StyledLinkWrapper>
+    </StyledTopPage>
   );
 };
+
+const StyledTopPage = styled.div`
+  min-height: 100vh;
+  /* Avoid Chrome to see Safari hack */
+  @supports (-webkit-touch-callout: none) {
+    min-height: -webkit-fill-available;
+  }
+  display: grid;
+  grid-template-rows: 1fr 1fr;
+  text-align: center;
+`;
+
+const StyledLinkWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 40px;
+`;
