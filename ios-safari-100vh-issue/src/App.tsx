10ec7d6dbad41f7c0dca37aad811f3151e2614ca
Shuji Honda <22165429+hondash@users.noreply.github.com>
2020-08-17 09:51:05 +0900

    [ios-safari-100vh-issue] move styling

--- ios-safari-100vh-issue/src/App.tsx
+++ ios-safari-100vh-issue/src/App.tsx
@@ -1,37 +1,23 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import styled from 'styled-components';
 
 import './App.css';
 import { FlexPage } from './pages/flex/FlexPage';
-// import { GridPage } from './pages/grid/GridPage';
+import { GridPage } from './pages/grid/GridPage';
 import { TopPage } from './pages/top/TopPage';
 
 export const App = () => {
   return (
-    <StyledApp>
-      <Switch>
-        <Route path="/flex">
-          <FlexPage />
-        </Route>
-        {/*<Route path="/grid">*/}
-        {/*  <GridPage />*/}
-        {/*</Route>*/}
-        <Route path="*">
-          <TopPage />
-        </Route>
-      </Switch>
-    </StyledApp>
+    <Switch>
+      <Route path="/flex">
+        <FlexPage />
+      </Route>
+      <Route path="/grid">
+        <GridPage />
+      </Route>
+      <Route path="*">
+        <TopPage />
+      </Route>
+    </Switch>
   );
 }
-
-const StyledApp = styled.div`
-  min-height: 100vh;
-  /* Avoid Chrome to see Safari hack */
-  @supports (-webkit-touch-callout: none) {
-    min-height: -webkit-fill-available;
-  }
-  text-align: center;
-  display: flex;
-  flex-direction: column;
-`
--- ios-safari-100vh-issue/src/pages/flex/FlexPage.tsx
+++ ios-safari-100vh-issue/src/pages/flex/FlexPage.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import styled from 'styled-components';
 
 import { Header } from './Header';
 import { Contents } from './Contents';
@@ -6,10 +7,21 @@ import { Footer } from './Footer';
 
 export const FlexPage = () => {
   return (
-    <>
+    <StyledApp>
       <Header />
       <Contents />
       <Footer />
-    </>
+    </StyledApp>
   )
 };
+
+const StyledApp = styled.div`
+  min-height: 100vh;
+  /* Avoid Chrome to see Safari hack */
+  @supports (-webkit-touch-callout: none) {
+    min-height: -webkit-fill-available;
+  }
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+`
