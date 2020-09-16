49d50dfb6c8d1700a8e9a1bb97abe698b8156a3f
Shuji Honda <22165429+hondash@users.noreply.github.com>
2020-09-17 00:53:14 +0900

    [query-to-object] add

--- frontend/query-to-object/src/index.ts
+++ frontend/query-to-object/src/index.ts
@@ -1,7 +1,26 @@
-import { parse } from 'querystring';
+import { parse, ParsedUrlQuery } from 'querystring';
 
-export const parseQueryToObject = (query: string) => {
+const parseQueryToObject = (query: string): ParsedUrlQuery => {
   if (query.startsWith('?')) {
     return parse(query.replace('?', ''));
   }
+  return parse(query);
 };
+
+interface ExpectedQuery {
+  aaa: string;
+  bbb: number;
+}
+
+const query = 'aaa=hoge';
+const obj = parseQueryToObject(query);
+
+const { aaa, bbb } = {
+  aaa: typeof obj.aaa === 'string' ? obj.aaa : undefined,
+  bbb:
+    typeof obj.bbb === 'string' && !Number.isNaN(parseInt(obj.bbb))
+      ? parseInt(obj.bbb)
+      : undefined,
+} as ExpectedQuery;
+
+console.log(aaa, bbb);
